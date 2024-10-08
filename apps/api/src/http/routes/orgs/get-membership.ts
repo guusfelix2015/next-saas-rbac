e0afc12714c3ea84194b117017af14c6ba1c23d2
guusfelix2015 <gusfelix2015@gmail.com>
2024-10-07 21:18:10 -0300

    obtendo afiliação

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+import { rolesSchema } from '../../../../../../packages/auth/src/roles'
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/membership',
+      {
+        schema: {
+          tags: ['Organization'],
+          summary: 'Get membership of on organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                role: rolesSchema,
+                id: z.string().uuid(),
+                organizationId: z.string(),
+              }),
+            }),
+          },
+        },
+      },
+
+      async (request) => {
+        const { slug } = request.params
+
+        const { membership } = await request.getUserMemberShip(slug)
+
+        return {
+          membership: {
+            role: membership.role,
+            id: membership.id,
+            organizationId: membership.organizationId,
+          },
+        }
+      },
+    )
+}
